ccf437fc634f28d47a632eb7d5234083f48560db
sorenara <fusanfilm@163.com>
2024-01-08 22:32:30 +0800

    adding spring profiles

---
+++ src/main/java/guru/springframework/spring6di/controllers/i18/Myi18NController.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6di.controllers.i18;
+
+import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Myi18NController {
+
+    private final GreetingService greetingService;
+
+    @Autowired
+    public Myi18NController(@Qualifier("i18NService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
